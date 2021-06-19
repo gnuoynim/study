18f814246bf7fdaed147fa8b8e5169a81b516953
kimminyoung <arome0825@naver.com>
2021-06-19 15:25:45 +0900

    hi

--- Doit_JS-master/homework/05/01/05_1.js
+++
@@ -1,7 +0,0 @@
-function checkNum(){
-    let num1 = Number(document.getElementsByTagName("input")[0].value);
-    let num2 = Number(document.getElementsByTagName("input")[1].value);
-
-    document.getElementById("e").value = (num1===num2) ? num1 * num2 : num1 + num2;
-    console.log(document.getElementById("e").value);
-}
